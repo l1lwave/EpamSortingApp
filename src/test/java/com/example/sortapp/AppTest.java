31d85eec942c179a9e4be1bff2c0cf24a42c0eee
Putintsev_Kirill <135183716+l1lwave@users.noreply.github.com>
2025-02-15 01:58:21 +0200

    Update AppTest.java

--- src/test/java/com/example/sortapp/AppTest.java
+++ src/test/java/com/example/sortapp/AppTest.java
@@ -1,4 +1,35 @@
 package com.example.sortapp;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class AppTest {
+    private String[] input;
+    private String expectedOutput;
+
+    public AppTest(String[] input, String expectedOutput) {
+        this.input = input;
+        this.expectedOutput = expectedOutput;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new String[]{"3", "1", "2"}, "[1, 2, 3]"},
+                {new String[]{"9"}, "[9]"},
+                {new String[]{}, "[]"},
+                {new String[]{"10", "9", "8", "7", "6", "5", "4", "3", "2", "1"}, "[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"},
+        });
+    }
+
+    @Test
+    public void testSortingApp() {
+        assertEquals(expectedOutput, Arrays.toString(Arrays.stream(input).mapToInt(Integer::parseInt).sorted().toArray()));
+    }
 }
